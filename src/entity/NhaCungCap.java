56476e112340621bf02b46ca7b13c8f227349c7f
Trần Vũ <vutkd23405@gmail.com>
2024-09-25 10:05:49 +0700

    Vũ commit lần 2

---
+++ src/entity/NhaCungCap.java
@@ -0,0 +1,72 @@
+package entity;
+
+import java.util.Objects;
+
+public class NhaCungCap {
+    private String maNCC;
+    private String tenNCC;
+    private String diaChi;
+    private String email;
+
+    public NhaCungCap(String maNCC, String tenNCC, String diaChi, String email) {
+        this.maNCC = maNCC;
+        this.tenNCC = tenNCC;
+        this.diaChi = diaChi;
+        this.email = email;
+    }
+
+    public String getMaNCC() {
+        return maNCC;
+    }
+
+    public void setMaNCC(String maNCC) {
+        this.maNCC = maNCC;
+    }
+
+    public String getTenNCC() {
+        return tenNCC;
+    }
+
+    public void setTenNCC(String tenNCC) {
+        this.tenNCC = tenNCC;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NhaCungCap that = (NhaCungCap) o;
+        return Objects.equals(maNCC, that.maNCC) && Objects.equals(tenNCC, that.tenNCC) && Objects.equals(diaChi, that.diaChi) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maNCC, tenNCC, diaChi, email);
+    }
+
+    @Override
+    public String toString() {
+        return "NhaCungCap{" +
+                "maNCC='" + maNCC + '\'' +
+                ", tenNCC='" + tenNCC + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
