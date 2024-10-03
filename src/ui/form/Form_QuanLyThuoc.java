aeb7dffee57fffc38160e540a7297e934444e186
PPY2204/HTCN_Web <nguyenvandu222k4@gmail.com>
2024-10-03 13:26:12 +0700

    update FormQuanLiThuoc and fix panel

--- src/ui/form/Form_QuanLyThuoc.java
+++ src/ui/form/Form_QuanLyThuoc.java
@@ -4,19 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Form_QuanLyThuoc  extends JPanel {
+    private final JButton btnAdd;
+    private final JButton btnUpdate;
+    private final JButton btnDelete;
+    private final JButton btnReload;
+    public JTextField txtSearch;
+    public JComboBox<String> cbNhaSanXuat, cbDanhMuc;
 
     public Form_QuanLyThuoc() {
         //Set layout NORTH
         JPanel pContainerNorth = new JPanel();
         pContainerNorth.setLayout(new BorderLayout());
-
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
         // Button back in NORTH
         JPanel pBack = new JPanel(new FlowLayout(FlowLayout.LEFT));
         ImageIcon arrowLeft = new ImageIcon("images/arrow_left.png");
         JButton btnBack = new JButton(arrowLeft);
         btnBack.setText("Quay lại");
-        pBack.setPreferredSize(new Dimension(100, 50));
+        pBack.setPreferredSize(new Dimension(110, 50));
         pBack.add(btnBack);
 
         // Title in NORTH
@@ -25,16 +31,63 @@ public class Form_QuanLyThuoc  extends JPanel {
         lblTitle.setFont(new Font("Arial", Font.BOLD, 50));
         pTitle.add(lblTitle);
 
-        pContainerNorth.setBackground(Color.RED);
-        pBack.setBackground(Color.BLUE);
-        pTitle.setBackground(Color.GREEN);
-
+        pContainerNorth.setPreferredSize(new Dimension(screenSize.width, pTitle.getPreferredSize().height));
+        pBack.setBackground(Color.WHITE);
         pContainerNorth.add(pBack, BorderLayout.WEST);
         pContainerNorth.add(pTitle, BorderLayout.CENTER);
 
-        this.add(pContainerNorth);
-
         // Set layout CENTER
+        JPanel pContainerCenter = new JPanel();
+        Box pOption = Box.createHorizontalBox();
+
+        // Search
+        pOption.add(txtSearch = new JTextField(30));
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // ComboBox Nhà sản xuất
+        String[] listNhaSanXuat = {"Nhà sản xuất","Nhà sản xuaất 1"};
+        cbNhaSanXuat = new JComboBox<>(listNhaSanXuat);
+        pOption.add(cbNhaSanXuat);
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // ComboBox Danh mục
+        String[] listDanhMuc = {"Danh mục","Đau đầu","Trĩ"};
+        cbDanhMuc = new JComboBox<>(listDanhMuc);
+        pOption.add(cbDanhMuc);
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // Add product
+        pOption.add(btnAdd = new JButton("+ Thêm"));
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // Update product
+        ImageIcon iconUpdate = new ImageIcon("images/update.png");
+        btnUpdate = new JButton(iconUpdate);
+        btnUpdate.setText("Cập nhật");
+        pOption.add(btnUpdate);
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // Delete product
+        ImageIcon iconDelete = new ImageIcon("images/delete.png");
+        btnDelete = new JButton(iconDelete);
+        btnDelete.setText("Xoá thuốc");
+        pOption.add(btnDelete);
+        pOption.add(Box.createHorizontalStrut(10));
+
+        // Delete product
+        ImageIcon iconReload = new ImageIcon("images/reload.png");
+        btnReload = new JButton(iconReload);
+        btnReload.setText("Làm mới");
+        pOption.add(btnReload);
+        pOption.add(Box.createHorizontalStrut(10));
+
+        pContainerCenter.add(pOption);
+
+        setLayout(new BorderLayout());
+        this.add(pContainerNorth, BorderLayout.NORTH);
+        this.add(pContainerCenter, BorderLayout.CENTER);
+
+
 
     }
 }
